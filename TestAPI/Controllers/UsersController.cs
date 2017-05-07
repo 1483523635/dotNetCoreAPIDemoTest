06085efce704089963de0515b66db668c42234ae
曲廉卿 <1483523635@qq.com>
2017-05-07 18:43:26 +0800

    用HttpClient实现了数据的提交替换了WebRequest

--- TestAPI/Controllers/UsersController.cs
+++ TestAPI/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TestAPI.Models;
@@ -18,16 +18,19 @@ namespace TestAPI.Controllers
     public class UsersController : Controller
     {
         #region 成员变量
-
+        //这个baseURL是我的webApi的地址
         private static string baseURL = "http://localhost:56853/api/users";
+        //用于存放所有的用户
         private static IList<Users> _context;
-        static HttpClient client = new HttpClient();
+
         #endregion
 
         #region 构造函数
         public UsersController()
         {
+            //实例化对象
             _context = new List<Users>();
+            //获取所有的用户的信息
             _context = GetAllUserList();
 
         }
@@ -36,19 +39,16 @@ namespace TestAPI.Controllers
 
         #region 类内方法
 
-       
-        ///  https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client
-      
-
-
         /// <summary>
         /// 通过api获取所有的用户的信息
         /// </summary>
         /// <returns>JSON String</returns>
         private string GetALLUserInfoFromAPI()
         {
+            HttpClient client = new HttpClient();
             var con = client.GetStringAsync(baseURL);
             return con.Result;
+
         }
 
         private string UsersConvertToJson(Users u)
@@ -56,25 +56,33 @@ namespace TestAPI.Controllers
             return JsonConvert.SerializeObject(u);
         }
         /// <summary>
-        /// 添加数据到数据库中
+        /// 传递数据到远程API的数据库中
         /// </summary>
         /// <param name="u"></param>
-        /// <returns>添加的一条数据json串</returns>
+        /// <returns></returns>
         private async Task<string> CreateAsync(Users u)
         {
             string url = baseURL;
-            string requestMethod = "post";
-            return await HandleData(u, url, requestMethod);         
+              string requestMethod = "Post";
+            //return await HandleDataAsync(u, url, requestMethod);
+            return await HandleDataFactoryAsync(u, url, requestMethod);
         }
 
         private async Task<string> UpdateAsync(Users u)
         {
             string url = baseURL + @"/" + u.ID;
-            string requestMethod = "put";
-            return await HandleData(u, url, requestMethod);
+            string requestMethod = "Put";
+           // return await HandleDataAsync(u, url, requestMethod);
+            return await HandleDataFactoryAsync(u, url, requestMethod);
         }
-
-        private async Task<string> HandleData(Users Data, string Url, string RequestMethod)
+        /// <summary>
+        /// 处理数据
+        /// </summary>
+        /// <param name="Data">User实体类</param>
+        /// <param name="Url">远程API的URL</param>
+        /// <param name="RequestMethod">请求的方法</param>
+        /// <returns></returns>
+        private async Task<string> HandleDataAsync(Users Data, string Url, string RequestMethod)
         {
             string UsersJson = UsersConvertToJson(Data);
             var request = WebRequest.CreateHttp(Url);
@@ -98,14 +106,65 @@ namespace TestAPI.Controllers
         private async Task<string> DeleteAsync(Users u)
         {
             string url = baseURL + @"/" + u.ID;
-            string requestMethod = "delete";
-            return await HandleData(u, url, requestMethod);
+            string requestMethod = "Delete";
+            //return await HandleDataAsync(u, url, requestMethod);
+            return await HandleDataFactoryAsync(u, url, requestMethod);
+
         }
         private bool UsersExist(int iD)
         {
             return _context.Any(u => u.ID == iD);
         }
 
+        /// <summary>
+        /// 这是用HttpClient调用Web Api实现CRUD到数据库中 
+        /// </summary>
+        /// <param name="u">数据</param>
+        /// <param name="url">请求的url</param>
+        /// <param name="RequestType">请求的类型</param>
+        /// <returns></returns>
+        private async Task<string> HandleDataFactoryAsync(Users u, string url, string RequestType)
+        {
+            //声明一个HttpClient类
+            HttpClient client = new HttpClient();
+            //将Users转化为JSon字符串
+            string jsonString = UsersConvertToJson(u);
+            //声明一个String类型的Context在提交数据时需要用到这个参数
+            var context = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            //设置反射需要获取的方法名称
+            string method = RequestType + "Async";
+
+            //我这里将两步写在了一起
+            //1.通过获取所有的方法
+            //2.通过linq语句查询我需要的方法
+            //当然也可以直接GetMethod(MethodName)
+            //我感觉那样的话可能会抛“方法找不到”的异常
+            //我用Linq查询避免了
+            MethodInfo info = client.GetType().GetMethods().FirstOrDefault(m => m.Name == method);
+            if (info != null)
+            {
+                HttpResponseMessage result;
+
+                //由于put和post需要的参数类型和数目一致所以放在一块处理了 
+                //get和delete也一样
+                if (method == "PutAsync" || method == "PostAsync")
+                {
+                    //激活这个方法并且传递所需要的参数
+                     result= await (Task<HttpResponseMessage>)info.Invoke(client, new Object[] { url, context });   
+                }
+                else
+                {
+                    result = await (Task<HttpResponseMessage>)info.Invoke(client, new Object[] { url });
+                }
+                //将响应内容返回
+                return await result.Content.ReadAsStringAsync();
+
+            }
+
+
+            return "";
+
+        }
         /// <summary>
         /// 获取所有用户的List
         /// </summary>
@@ -178,7 +237,7 @@ namespace TestAPI.Controllers
         {
             if (ModelState.IsValid)
             {
-               
+
                 await CreateAsync(users);
                 return RedirectToAction("Index");
             }
